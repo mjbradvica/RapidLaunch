3a30bb476afe6f82c9f84a9d179cfa6ccb91bf8e
Michael B <mjbradvica@users.noreply.github.com>
2022-07-10 13:54:41 -0400

    updated base ado

--- source/NBaseRepository.ADO/BaseRepository.cs
+++ source/NBaseRepository.ADO/BaseRepository.cs
@@ -8,41 +8,31 @@
     using Common;
     using SQL;
 
-    public abstract class BaseRepository<T, TId> :
-        IGetAllEntities<T, TId>,
-        IGetAllEntitiesAsync<T, TId>
-        where T : IEntity<TId>
+    public abstract class BaseRepository<TEntity, TId> :
+        IGetAllEntities<TEntity, TId>,
+        IGetAllEntitiesAsync<TEntity, TId>
+        where TEntity : IEntity<TId>
         where TId : struct
     {
-        private readonly SqlBuilder<T, TId> _sqlBuilder;
+        private readonly SqlBuilder<TEntity, TId> _sqlBuilder;
         private readonly SqlConnection _sqlConnection;
-        private readonly Func<IEnumerable<object>, T> _conversionFunc;
+        private readonly Func<SqlDataReader, TEntity> _conversionFunc;
 
-        protected BaseRepository(SqlBuilder<T, TId> sqlBuilder, SqlConnection sqlConnection, Func<IEnumerable<object>, T> conversionFunc)
+        protected BaseRepository(SqlBuilder<TEntity, TId> sqlBuilder, SqlConnection sqlConnection, Func<SqlDataReader, TEntity> conversionFunc)
         {
             _sqlBuilder = sqlBuilder;
             _sqlConnection = sqlConnection;
             _conversionFunc = conversionFunc;
         }
 
-        public virtual IReadOnlyList<T> GetAllEntities()
+        public virtual IReadOnlyList<TEntity> GetAllEntities()
         {
             return ExecuteQuery(_sqlBuilder.SelectAll().Query);
         }
 
-        public virtual IReadOnlyList<T> GetAllEntities(Func<IEnumerable<object>, T> includeFunc)
-        {
-            return ExecuteQuery(_sqlBuilder.SelectAll().Query, includeFunc);
-        }
-
-        public virtual async Task<IReadOnlyList<T>> GetAllEntitiesAsync(CancellationToken cancellationToken = default)
+        public virtual async Task<IReadOnlyList<TEntity>> GetAllEntitiesAsync(CancellationToken cancellationToken = default)
         {
             return await ExecuteQueryAsync(_sqlBuilder.SelectAll().Query, default, cancellationToken);
-        }
-
-        public virtual async Task<IReadOnlyList<T>> GetAllEntitiesAsync(Func<IEnumerable<object>, T> includeFunc, CancellationToken cancellationToken = default)
-        {
-            return await ExecuteQueryAsync(_sqlBuilder.SelectAll().Query, includeFunc, cancellationToken);
         }
 
         protected int ExecuteNonQuery(string command)
@@ -62,16 +52,33 @@
         {
             await _sqlConnection.OpenAsync(cancellationToken);
 
+            var transaction = await _sqlConnection.BeginTransactionAsync(cancellationToken);
+
             var sqlCommand = new SqlCommand(command, _sqlConnection);
 
-            var result = await sqlCommand.ExecuteNonQueryAsync(cancellationToken);
+            int result;
 
-            await _sqlConnection.CloseAsync();
+            try
+            {
+                result = await sqlCommand.ExecuteNonQueryAsync(cancellationToken);
+
+                await transaction.CommitAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                await transaction.RollbackAsync(cancellationToken);
+
+                throw;
+            }
+            finally
+            {
+                await _sqlConnection.CloseAsync();
+            }
 
             return result;
         }
 
-        protected List<T> ExecuteQuery(string command, Func<IEnumerable<object>, T> overloadDefaultConversion = default)
+        protected List<TEntity> ExecuteQuery(string command, Func<SqlDataReader, TEntity> overloadDefaultConversion = default)
         {
             var sqlQuery = new SqlCommand(command, _sqlConnection);
 
@@ -79,20 +86,13 @@
 
             var sqlDataReader = sqlQuery.ExecuteReader();
 
-            var result = new List<T>();
+            var result = new List<TEntity>();
 
             var conversionFunc = overloadDefaultConversion ?? _conversionFunc;
 
             while (sqlDataReader.Read())
             {
-                var rowObjects = new List<object>();
-
-                for (int i = 0; i < sqlDataReader.FieldCount; i++)
-                {
-                    rowObjects.Add(sqlDataReader[i]);
-                }
-
-                result.Add(conversionFunc.Invoke(rowObjects));
+                result.Add(conversionFunc.Invoke(sqlDataReader));
             }
 
             _sqlConnection.Close();
@@ -100,7 +100,7 @@
             return result;
         }
 
-        protected async Task<List<T>> ExecuteQueryAsync(string command, Func<IEnumerable<object>, T> overloadDefaultConversion = default, CancellationToken cancellationToken = default)
+        protected async Task<List<TEntity>> ExecuteQueryAsync(string command, Func<SqlDataReader, TEntity> overloadDefaultConversion = default, CancellationToken cancellationToken = default)
         {
             var sqlQuery = new SqlCommand(command, _sqlConnection);
 
@@ -108,20 +108,13 @@
 
             var sqlDataReader = await sqlQuery.ExecuteReaderAsync(cancellationToken);
 
-            var result = new List<T>();
+            var result = new List<TEntity>();
 
             var conversionFunc = overloadDefaultConversion ?? _conversionFunc;
 
             while (await sqlDataReader.ReadAsync(cancellationToken))
             {
-                var rowObjects = new List<object>();
-
-                for (int i = 0; i < sqlDataReader.FieldCount; i++)
-                {
-                    rowObjects.Add(sqlDataReader[i]);
-                }
-
-                result.Add(conversionFunc.Invoke(rowObjects));
+                result.Add(conversionFunc.Invoke(sqlDataReader));
             }
 
             await _sqlConnection.CloseAsync();
