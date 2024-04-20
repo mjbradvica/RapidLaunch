ef77a57369cf21fdbbd84d47bf85a110bd28632b
Michael B <mjbradvica@gmail.com>
2024-04-20 14:55:45 -0400

    test updates

--- tests/RapidLaunch.EF.Tests/RapidLaunchRepositoryTests.cs
+++ tests/RapidLaunch.EF.Tests/RapidLaunchRepositoryTests.cs
@@ -17,11 +17,40 @@ namespace RapidLaunch.EF.Tests
     public class RapidLaunchRepositoryTests : BaseIntegrationTest
     {
         /// <summary>
-        /// Can add entities correct.
+        /// Can add entities correctly.
+        /// </summary>
+        [TestMethod]
+        public void AddEntities_IsCorrect()
+        {
+            var entities = new List<TestEntity>
+            {
+                new TestEntity(),
+                new TestEntity(),
+            };
+
+            using (var context = new TestDbContext())
+            {
+                var repo = new TestRepository(context);
+
+                repo.AddEntities(entities);
+            }
+
+            using (var context = new TestDbContext())
+            {
+                var repo = new TestRepository(context);
+
+                var result = repo.GetAllEntities();
+
+                Assert.AreEqual(entities.Count, result.Count);
+            }
+        }
+
+        /// <summary>
+        /// Can add entities async correctly.
         /// </summary>
         /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
         [TestMethod]
-        public async Task AddEntities_IsCorrect()
+        public async Task AddEntitiesAsync_IsCorrect()
         {
             var entities = new List<TestEntity>
             {
@@ -43,6 +72,53 @@ namespace RapidLaunch.EF.Tests
                 var result = await repo.GetAllEntitiesAsync();
 
                 Assert.AreEqual(entities.Count, result.Count);
+            }
+        }
+
+        /// <summary>
+        /// Can add an entity correctly.
+        /// </summary>
+        [TestMethod]
+        public void AddEntity_IsCorrect()
+        {
+            using (var context = new TestDbContext())
+            {
+                var repo = new TestRepository(context);
+
+                repo.AddEntity(new TestEntity());
+            }
+
+            using (var context = new TestDbContext())
+            {
+                var repo = new TestRepository(context);
+
+                var result = repo.GetAllEntities();
+
+                Assert.AreEqual(1, result.Count);
+            }
+        }
+
+        /// <summary>
+        /// Can add an entity async correctly.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous unit test.</returns>
+        [TestMethod]
+        public async Task AddEntityAsync_IsCorrect()
+        {
+            await using (var context = new TestDbContext())
+            {
+                var repo = new TestRepository(context);
+
+                await repo.AddEntityAsync(new TestEntity());
+            }
+
+            await using (var context = new TestDbContext())
+            {
+                var repo = new TestRepository(context);
+
+                var result = await repo.GetAllEntitiesAsync();
+
+                Assert.AreEqual(1, result.Count);
             }
         }
     }
