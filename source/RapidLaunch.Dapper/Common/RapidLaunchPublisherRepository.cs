898147c2bd75f83c35bf65ba6ed64b92d62ae5f5
Michael Bradvica <mjbradvica@gmail.com>
2024-04-30 20:39:36 -0400

    fixed merge issues

--- source/RapidLaunch.Dapper/Common/RapidLaunchPublisherRepository.cs
+++ source/RapidLaunch.Dapper/Common/RapidLaunchPublisherRepository.cs
@@ -9,14 +9,13 @@ using RapidLaunch.Common;
 namespace RapidLaunch.Dapper.Common
 {
     /// <inheritdoc />
-    public abstract class RapidLaunchPublisherRepository<TModel, TEntity, TId> : RapidLaunchRepository<TModel, TEntity, TId>
-        where TModel : class
+    public abstract class RapidLaunchPublisherRepository<TEntity, TId> : RapidLaunchRepository<TEntity, TId>
         where TEntity : class, IAggregateRoot<TId>
     {
         private readonly IPublishingBus _publishingBus;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="RapidLaunchPublisherRepository{TModel, TEntity, TId}"/> class.
+        /// Initializes a new instance of the <see cref="RapidLaunchPublisherRepository{TEntity, TId}"/> class.
         /// </summary>
         /// <param name="connection">An instance of the <see cref="SqlConnection"/> class.</param>
         /// <param name="publishingBus">An instance of the <see cref="IPublishingBus"/> interface.</param>
