c14b71c2a7599f77e57d7ad5f5bcc4c3bfdb252d
Michael Bradvica <mjbradvica@gmail.com>
2024-05-13 16:48:58 -0400

    test coverage update

---
+++ tests/RapidLaunch.Tests/Common/TestNotification.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="TestNotification.cs" company="Wayne John Whistler LLC">
+// Copyright (c) Wayne John Whistler LLC. All rights reserved.
+// </copyright>
+
+using ClearDomain.Common;
+
+namespace RapidLaunch.Tests.Common
+{
+    /// <summary>
+    /// Sample class for notifications.
+    /// </summary>
+    public class TestNotification : IDomainEvent
+    {
+    }
+}
