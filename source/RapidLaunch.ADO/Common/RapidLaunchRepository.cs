87e095c53f7bd308889c50f4123374a07cbf23ed
Michael B <mjbradvica@gmail.com>
2024-10-01 19:10:15 -0400

    add stored proc

--- source/RapidLaunch.ADO/Common/RapidLaunchRepository.cs
+++ source/RapidLaunch.ADO/Common/RapidLaunchRepository.cs
@@ -4,8 +4,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ClearDomain.Common;
@@ -18,8 +18,7 @@ namespace RapidLaunch.ADO.Common
     /// </summary>
     /// <typeparam name="TEntity">The type of the entity.</typeparam>
     /// <typeparam name="TId">The type of the identifier.</typeparam>
-    public abstract class RapidLaunchRepository<TEntity, TId> :
-        IAddEntitiesAsync<TEntity, TId>
+    public abstract class RapidLaunchRepository<TEntity, TId>
         where TEntity : class, IAggregateRoot<TId>
     {
         private readonly SqlConnection _sqlConnection;
@@ -39,26 +38,13 @@ namespace RapidLaunch.ADO.Common
             _conversionFunc = conversionFunc;
         }
 
-/// <inheritdoc/>
-        public virtual async Task<RapidLaunchStatus> AddEntitiesAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
-        {
-            return await ExecuteCommandAsync(
-                () =>
-                {
-                    var asList = entities.ToList();
-
-                    return (_sqlBuilder.InsertMultiple(asList).SqlStatement, asList);
-                },
-                cancellationToken);
-        }
-
         /// <summary>
         /// Executes a non-query synchronously.
         /// </summary>
         /// <param name="executionFunc">A <see cref="Func{TResult}"/> that yields a SQL statement and the entities to execute again.</param>
         /// <param name="postOperationFunc">A <see cref="Func{TResult}"/> to run post operation effects.</param>
         /// <returns>A <see cref="RapidLaunchStatus"/> that indicates the number of rows affected.</returns>
-        protected virtual RapidLaunchStatus ExecuteCommand(Func<(string Command, IEnumerable<TEntity> Entities)> executionFunc,  Action<int, IEnumerable<IAggregateRoot<TId>>>? postOperationFunc = default)
+        protected virtual RapidLaunchStatus ExecuteCommand(Func<(string Command, IEnumerable<TEntity> Entities)> executionFunc, Action<int, IEnumerable<IAggregateRoot<TId>>>? postOperationFunc = default)
         {
             int result;
 
@@ -148,9 +134,8 @@ namespace RapidLaunch.ADO.Common
         /// </summary>
         /// <param name="command">The sql command to execute.</param>
         /// <param name="overloadDefaultConversion">An overload to use a custom function.</param>
-        /// <param name="postExecutionFunc">A <see cref="Func{TResult}"/> to modify the result set if required.</param>
         /// <returns>A <see cref="Task{TResult}"/> of type <see cref="List{TResult}"/> representing the result of the asynchronous operation.</returns>
-        protected virtual List<TEntity> ExecuteQuery(string command, Func<SqlDataReader, TEntity>? overloadDefaultConversion = default, Func<List<TEntity>, List<TEntity>>? postExecutionFunc = default)
+        protected virtual List<TEntity> ExecuteQuery(string command, Func<SqlDataReader, TEntity>? overloadDefaultConversion = default)
         {
             var sqlQuery = new SqlCommand(command, _sqlConnection);
 
@@ -169,11 +154,6 @@ namespace RapidLaunch.ADO.Common
 
             _sqlConnection.Close();
 
-            if (postExecutionFunc != null)
-            {
-                return postExecutionFunc.Invoke(result);
-            }
-
             return result;
         }
 
@@ -182,10 +162,9 @@ namespace RapidLaunch.ADO.Common
         /// </summary>
         /// <param name="command">The sql command to execute.</param>
         /// <param name="overloadDefaultConversion">An overload to use a custom conversion function.</param>
-        /// /// <param name="postExecutionFunc">A <see cref="Func{TResult}"/> to modify the result set if required.</param>
         /// <param name="cancellationToken">A <see cref="CancellationToken"/>.</param>
         /// <returns>A <see cref="Task{TResult}"/> of type <see cref="List{TResult}"/> representing the result of the asynchronous operation.</returns>
-        protected virtual async Task<List<TEntity>> ExecuteQueryAsync(string command, Func<SqlDataReader, TEntity>? overloadDefaultConversion = default, Func<List<TEntity>, List<TEntity>>? postExecutionFunc = default, CancellationToken cancellationToken = default)
+        protected virtual async Task<List<TEntity>> ExecuteQueryAsync(string command, Func<SqlDataReader, TEntity>? overloadDefaultConversion = default, CancellationToken cancellationToken = default)
         {
             var sqlQuery = new SqlCommand(command, _sqlConnection);
 
@@ -204,66 +183,50 @@ namespace RapidLaunch.ADO.Common
 
             await _sqlConnection.CloseAsync();
 
-            if (postExecutionFunc != null)
-            {
-                return postExecutionFunc.Invoke(result);
-            }
-
             return result;
         }
 
-        // TODO: Condense using the ExecuteQuery method.
-        private List<string> GetColumnNames()
+        /// <summary>
+        /// Executes a stored procedure for a query.
+        /// </summary>
+        /// <param name="procedureName">The name of procedure to run.</param>
+        /// <param name="parameters">A <see cref="Dictionary{TKey,TValue}"/> of parameters to pass.</param>
+        /// <param name="overloadDefaultConversion">A <see cref="Func{TResult}"/> to overload the data reader.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/>.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        protected virtual async Task<List<TEntity>> ExecuteStoredProcedureQueryAsync(
+            string procedureName,
+            Dictionary<string, object>? parameters = default,
+            Func<SqlDataReader, TEntity>? overloadDefaultConversion = default,
+            CancellationToken cancellationToken = default)
         {
-            if (_sqlBuilder.ColumnNames != null)
+            var command = new SqlCommand(procedureName, _sqlConnection)
             {
-                return _sqlBuilder.ColumnNames.ToList();
-            }
+                CommandType = CommandType.StoredProcedure,
+            };
 
-            var sqlQuery = new SqlCommand(_sqlBuilder.GetColumnNames().SqlStatement, _sqlConnection);
-
-            _sqlConnection.Open();
-
-            var sqlDataReader = sqlQuery.ExecuteReader();
-
-            var result = new List<string>();
-
-            while (sqlDataReader.Read())
+            if (parameters != null)
             {
-                result.Add(sqlDataReader.GetString(0));
+                foreach (var parameter in parameters)
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
             }
-
-            _sqlConnection.Close();
-
-            _sqlBuilder.UpdateColumnNames(result);
-
-            return result;
-        }
-
-        // TODO: Condense using the ExecuteQuery method.
-        private async Task<List<string>> GetColumnNamesAsync(CancellationToken cancellationToken = default)
-        {
-            if (_sqlBuilder.ColumnNames != null)
-            {
-                return _sqlBuilder.ColumnNames.ToList();
-            }
-
-            var sqlQuery = new SqlCommand(_sqlBuilder.GetColumnNames().SqlStatement, _sqlConnection);
 
             await _sqlConnection.OpenAsync(cancellationToken);
 
-            var sqlDataReader = await sqlQuery.ExecuteReaderAsync(cancellationToken);
+            var sqlDataReader = await command.ExecuteReaderAsync(cancellationToken);
 
-            var result = new List<string>();
+            var result = new List<TEntity>();
+
+            var conversionFunc = overloadDefaultConversion ?? _conversionFunc;
 
             while (await sqlDataReader.ReadAsync(cancellationToken))
             {
-                result.Add(sqlDataReader.GetString(0));
+                result.Add(conversionFunc.Invoke(sqlDataReader));
             }
 
-            _sqlConnection.Close();
-
-            _sqlBuilder.UpdateColumnNames(result);
+            await _sqlConnection.CloseAsync();
 
             return result;
         }
